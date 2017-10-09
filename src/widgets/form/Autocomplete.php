659bc6d064a4d2e3dd46c69d24273f51a9d886aa
Andriy Svirin <andriy.svirin@ukrinsoft.com>
2017-10-09 08:59:09 -0400

    Update autocomplete widget

--- src/widgets/form/Autocomplete.php
+++ src/widgets/form/Autocomplete.php
@@ -2,65 +2,67 @@
 
 namespace macgyer\yii2materializecss\widgets\form;
 
-use macgyer\yii2materializecss\lib\BaseInputWidget;
+use yii\helpers\Json;
 use yii\helpers\Html;
-use yii\helpers\Url;
-use macgyer\yii2materializecss\assets\AutocompleteAsset;
+use yii\web\JsExpression;
 
-class Autocomplete extends BaseInputWidget
+/**
+ * Autocomplete class.
+ */
+class Autocomplete extends \yii\base\Widget
 {
-    public $startQuery = true;
-    public $wrapperSelector;
-    public $url = [];
+
+    /**
+     * Element options.
+     * @var array 
+     */
     public $options = [];
 
-    private $_ajaxUrl;
+    /**
+     * Widget options.
+     * @var array 
+     */
+    public $clientOptions = [];
 
-    public function getUrl()
+    /**
+     * {inheritDoc}
+     */
+    public function init()
     {
-        if ($this->_ajaxUrl === null) {
-            $this->_ajaxUrl = Url::toRoute($this->url);
-        }
-        return $this->_ajaxUrl;
+        $id = $this->getId();
+        $this->clientOptions['appendTo'] = new JsExpression('jQuery("#wrapper-' . $id . '")');
+        parent::init();
     }
 
-    public function run(){
+    /**
+     * {inheritDoc}
+     */
+    public function getId($autoGenerate = true)
+    {
+        if (isset($this->options['id'])) {
+            return $this->options['id'];
+        }
+        return parent::getId($autoGenerate);
+    }
+
+    /**
+     * {inheritDoc}
+     */
+    public function run()
+    {
         $id = $this->getId();
-        $value = $this->model->{$this->attribute};
-
-        $this->getView()->registerJs("
-            var cache_{$id} = {};
-            var cache_{$id}_1 = {};
-            var cache_{$id}_2 = {};
-            $('#{$id}').autocomplete({
-                 appendTo: $('#{$id}').parent('.wrapper-{$id}'),
-                 width: 'auto',
-                 lookup: function (request, response) {
-                    var term = request;
-                    if ( term in cache_{$id} ) {
-                        response( cache_{$id} [term] );
-                        return;
-                    }
-                    $.getJSON('{$this->getUrl()}', {term: term}, function( data, status, xhr ) {
-                        cache_{$id} [term] = data;
-                        console.log(data);
-                        response(data);
-                    });
-                 }, 
-                 onSelect: function (suggestion) {
-                     $('#{$id}-hidden').val(suggestion.data);
-                     $('#{$id}-hidden').change();
-                 }
+        $options = empty($this->clientOptions) ? '' : Json::encode($this->clientOptions);
+        $this->getView()->registerJs(''
+            . 'jQuery(document).mouseup(function (e) {'
+            . '  var container = jQuery("#wrapper-' . $id . ' .autocomplete-suggestions");
+                  if (container.has(e.target).length === 0){container.hide();}
+               });
+            jQuery(document).keydown(function (e) {
+                var container = $("#wrapper-' . $id . ' .autocomplete-suggestions");
+                if (e.which === 27){container.hide();}   
             });
-        ");
-
-        return Html::tag('div',
-            Html::activeHiddenInput($this->model, $this->attribute, ['id' => $id . '-hidden', 'class' => 'form-control'])
-            . ($value && $this->startQuery ? Html::tag('div', "<img src='{$this->registerActiveAssets()}/images/load.gif'/>", ['class' => 'autocomplete-image-load']) : '')
-            . Html::textInput('', $value && !$this->startQuery ? $value : '', array_merge($this->options, ['id' => $id, 'class' => 'form-control']))
-            , [
-                'class' => 'wrapper-' . $id
-            ]
-        );
+            jQuery("#' . $id . '").autocomplete(' . $options . ');');
+        return Html::tag('div', Html::textInput('', '', array_merge($this->options, ['id' => $id, 'class' => 'form-control']))
+                , ['id' => 'wrapper-' . $id]);
     }
 }
